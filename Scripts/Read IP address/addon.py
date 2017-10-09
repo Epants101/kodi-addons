416c6f8a4ad2540f62dc7c858f66c023eed54475
Epants101 <32354350+Epants101@users.noreply.github.com>
2017-10-09 19:02:06 +1100

    Update addon.py

--- Scripts/Read IP address/addon.py
+++ Scripts/Read IP address/addon.py
@@ -1,26 +1,27 @@
 ip = str(xmbc.getIPAddress())
 for each in ip:
   if(each=="."):
-    xmbc.playSFX("resources/dot.wav")
+    xmbc.playSFX("resources/media/dot.wav")
   elif(each=="0"):
-    xmbc.playSFX("resources/zero.wav")
+    xmbc.playSFX("resources/media/zero.wav")
   elif(each=="1"):
-    xmbc.playSFX("resources/one.wav")
+    xmbc.playSFX("resources/media/one.wav")
   elif(each=="2"):
-    xmbc.playSFX("resources/two.wav")
+    xmbc.playSFX("resources/media/two.wav")
   elif(each=="3"):
-    xmbc.playSFX("resources/three.wav")
+    xmbc.playSFX("resources/media/three.wav")
   elif(each=="4"):
-    xmbc.playSFX("resources/four.wav")
+    xmbc.playSFX("resources/media/four.wav")
   elif(each=="5"):
-    xmbc.playSFX("resources/five.wav")
+    xmbc.playSFX("resources/media/five.wav")
   elif(each=="6"):
-    xmbc.playSFX("resources/six.wav")
+    xmbc.playSFX("resources/media/six.wav")
   elif(each=="7"):
-    xmbc.playSFX("resources/seven.wav")
+    xmbc.playSFX("resources/media/seven.wav")
   elif(each=="8"):
-    xmbc.playSFX("resources/eight.wav")
+    xmbc.playSFX("resources/media/eight.wav")
   elif(each=="9"):
-    xmbc.playSFX("resources/nine.wav")
+    xmbc.playSFX("resources/media/nine.wav")
   else:
     xmbc.log("Your IP address contains unknown characters.")
+  xmbc.sleep(1000)
